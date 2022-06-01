372089cb90ebc70b7e6dc3833e0bb3d522481b38
Anas Elgarhy <anas.elgarhy.dev@gmail.com>
2022-06-01 06:29:49 +0200

    Add delau between press key and release it

--- src/main/java/org/cuteviruses/deletewriteKeybord/KeyListener.java
+++ src/main/java/org/cuteviruses/deletewriteKeybord/KeyListener.java
@@ -3,14 +3,12 @@ package org.cuteviruses.deletewriteKeybord;
 import com.github.kwhat.jnativehook.keyboard.NativeKeyEvent;
 import com.github.kwhat.jnativehook.keyboard.NativeKeyListener;
 
-import java.awt.*;
 import java.awt.event.KeyEvent;
 
 public class KeyListener implements NativeKeyListener {
     public void nativeKeyTyped(NativeKeyEvent e) {
         MainController.robot.keyPress(KeyEvent.VK_BACK_SPACE);
+        MainController.robot.delay(100);
         MainController.robot.keyRelease(KeyEvent.VK_BACK_SPACE);
     }
-
-
 }
